5c6da5bdc5fb69af31e4aa6e28ac30fc3e4f5328
fabiopulvirenti <fpulvirenti002@gmail.com>
2024-09-05 11:09:43 +0200

    added main class and ApplicationTest class

---
+++ src/main/java/com/northcoders/recordShopApi/RecordShopApiApplication.java
@@ -0,0 +1,15 @@
+package com.northcoders.recordShopApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordShopApiApplication.class, args);
+	}
+
+}
+
+
